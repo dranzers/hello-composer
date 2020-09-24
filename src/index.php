a00c9d7e8257a2b2033221ddcdd5150bccfcbbb9
rahmad@its.ac.id <rahmad@its.ac.id>
2020-09-24 11:00:51 +0700

    initial library

---
+++ src/index.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dranzers\HelloComposer;
+
+class Index {
+    
+    public function _excecute(?String $name)
+    {
+        return 'Hello ' .$name. ', How are you ?';
+    }
+}
+
